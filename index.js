a3d1ab422610404082e41505dfca55a423c6a0c9
Christina Entcheva <christina.entcheva@gmail.com>
2016-10-31 21:57:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 function dwarfRollCall(dwarves) {
+
+  var str = "";
+
+  for (var i = 0; i < dwarves.length; i++) {
+    str += i+1 + ". " + dwarves[i] + " ";
+  }
+  return str;
 }
 
-function summonCaptainPlanet(planeteerCalls){
+
+
+function summonCaptainPlanet(arr) {
+  var upperCall = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    upperCall.push(arr[i].toUpperCase() + "!")
+  }
+  return upperCall;
 }
+
+
 
 function longPlaneteerCalls(words) {
+
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
+
+
 function findTheCheese (foods) {
+  var cheeses = ["cheddar", "gouda", "camembert"];
+
+  for (var i = 0; i < foods.length; i++) {
+
+    if (cheeses.indexOf(foods[i]) !== -1) {
+      return foods[i];
+    }
+  }
+  return "no cheese!";
 }
